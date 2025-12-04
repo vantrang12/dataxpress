94ade740f364d4b90e318360042cb6892365a0ac
vantrang12 <vantrang12@yahoo.com>
2025-12-04 09:46:33 +0700

    v2d

--- src/routes/organizations.js
+++ src/routes/organizations.js
@@ -12,5 +12,6 @@ router.get("/", getOrganizations);
 router.post("/", createOrganization);
 router.put("/:id", updateOrganization);
 router.delete("/:id", deleteOrganization);
+router.get('/:id', organizationsController.getOrganizationById);
 
 export default router;
